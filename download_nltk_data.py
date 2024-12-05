b62a6cd442795b245b58ff4f7e837851856bf617
Peter Park <fantasyindeath86@gmail.com>
2024-12-05 09:49:35 -0500

    Update download_nltk_data.py

--- download_nltk_data.py
+++ download_nltk_data.py
@@ -1,8 +1,11 @@
 import nltk
 import ssl
 import certifi
+import os
 
 ssl._create_default_https_context = ssl._create_unverified_context
 ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
 
-nltk.download('words', download_dir='/tmp/nltk_data')
+nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.download('words', download_dir=nltk_data_dir)
