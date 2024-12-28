1a9e6e34cc75567133f1f636d96b11153413a972
vostsoldier <fantasyindeath86@gmail.com>
2024-12-28 13:18:18 +0900

    maybe this will fix it

--- migrations/env.py
+++ migrations/env.py
@@ -17,15 +17,20 @@ logger = logging.getLogger('alembic.env')
 
 def get_engine():
     try:
+        # this works with Flask-SQLAlchemy<3 and Alchemical
         return current_app.extensions['migrate'].db.get_engine()
     except (TypeError, AttributeError):
+        # this works with Flask-SQLAlchemy>=3
         return current_app.extensions['migrate'].db.engine
+
 
 def get_engine_url():
     try:
-        return get_engine().url.render_as_string(hide_password=False).replace('%', '%%')
+        return get_engine().url.render_as_string(hide_password=False).replace(
+            '%', '%%')
     except AttributeError:
         return str(get_engine().url).replace('%', '%%')
+
 
 # add your model's MetaData object here
 # for 'autogenerate' support
